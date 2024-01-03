5289f89e8361212a0c82eb4a5ba8c901a8bb5262
KTDKasp <churroskasp@gmail.com>
2024-01-03 10:02:45 +0300

    Добавлен useEffect для получения данных из mokky

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,41 @@ import { PizzaBlock } from './components/PizzaBlock';
 
 import './scss/app.scss';
 
-import pizzas from './assets/pizza.json';
-
 function App() {
-	const [pizzaAmount, setPizzaAmount] = React.useState([]);
+  const [items, setItems] = React.useState([]);
 
-	return (
-		<div className="wrapper">
-			<Header />
-			<div className="content">
-				<div className="container">
-					<div className="content__top">
-						<Categories />
-						<Sort />
-					</div>
-					<h2 className="content__title">Все пиццы</h2>
-					<div className="content__items">
-						{pizzas.map((obj) => (
-							<PizzaBlock
-								key={obj.id}
-								imageUrl={obj.imageUrl}
-								title={obj.title}
-								price={obj.price}
-								sizes={obj.sizes}
-								types={obj.types}
-							/>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  React.useEffect(() => {
+    fetch('https://7f678c67a9e8e4e6.mokky.dev/items')
+      .then(res => res.json())
+      .then(data => setItems(data));
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((obj) => (
+              <PizzaBlock
+                key={obj.id}
+                imageUrl={obj.imageUrl}
+                title={obj.title}
+                price={obj.price}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

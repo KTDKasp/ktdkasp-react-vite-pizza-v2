e14b3011c1e052726542fd69ca6800950ac6241e
KTDKasp <churroskasp@gmail.com>
2024-01-11 22:28:11 +0300

    Добавили функционал поиска и пагинацию

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,14 @@ import { Categories } from '../components/Categories';
 import { Sort } from '../components/Sort';
 import { PizzaBlock } from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import { Pagination } from '../components/Pagination';
 
-export const Home = () => {
+export const Home = ({ searchValue }) => {
 	const [items, setItems] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
 	const [categoryId, setCategoryId] = React.useState(0);
+	const [currentPage, setCurrentPage] = React.useState(1);
+	const [pageMetaData, setPageMetaData] = React.useState({})
 	const [sortType, setSortType] = React.useState({
 		name: 'популярности',
 		sortProperty: 'rating',
@@ -17,15 +20,21 @@ export const Home = () => {
 	React.useEffect(() => {
 		setIsLoading(true);
 		fetch(
-			`https://7f678c67a9e8e4e6.mokky.dev/items?${
-				categoryId > 0 ? `category=${categoryId}` : ''
-			}&sortBy=${sortType.sortProperty}`
+			`https://7f678c67a9e8e4e6.mokky.dev/items?page=${currentPage}&limit=4${
+				categoryId > 0 ? `&category=${categoryId}` : ''
+			}&sortBy=${sortType.sortProperty}${
+				searchValue ? `&title=*${searchValue}*` : ''
+			}`
 		)
 			.then((res) => res.json())
-			.then((data) => setItems(data))
+			.then((data) => {
+				setItems(data.items);
+				setPageMetaData(data.meta);
+			})
 			.finally(() => setIsLoading(false));
 		window.scrollTo(0, 0);
-	}, [categoryId, sortType]);
+		console.log(pageMetaData);
+	}, [categoryId, sortType, searchValue, currentPage]);
 
 	return (
 		<div className="container">
@@ -53,6 +62,7 @@ export const Home = () => {
 							/>
 					  ))}
 			</div>
+			<Pagination onChangePage={number => setCurrentPage(number)}/>
 		</div>
 	);
 };

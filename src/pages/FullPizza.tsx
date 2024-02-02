65c3df235e64d00d65cf65c6627df53412a72438
KTDKasp <churroskasp@gmail.com>
2024-02-02 23:38:51 +0300

    Добавили 'кнопку назад на главную' для элемента FullPizza

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 interface pizzaData {
-	imageUrl: string,
-	title: string,
-	price: number,
+	imageUrl: string;
+	title: string;
+	price: number;
 }
 
 const FullPizza: React.FC = () => {
@@ -16,29 +16,36 @@ const FullPizza: React.FC = () => {
 	React.useEffect(() => {
 		const fetchPizzaData = async () => {
 			try {
-				const { data } = await axios.get("https://7f678c67a9e8e4e6.mokky.dev/items/" + id);
+				const { data } = await axios.get(
+					'https://7f678c67a9e8e4e6.mokky.dev/items/' + id
+				);
 				setPizza(data);
 			} catch (error) {
-				alert("Ошибка при получении пиццы!")
+				alert('Ошибка при получении пиццы!');
 				navigate('/');
 			}
-		}
+		};
 		fetchPizzaData();
-	}, [])
+	}, []);
 
 	if (!pizza) {
 		return (
-			<div className='container'>
+			<div className="container">
 				<h2>Loading...</h2>
 			</div>
 		);
 	}
 
 	return (
-		<div className='container'>
+		<div className="container">
 			<img src={pizza.imageUrl} alt="name" />
 			<h2>{pizza.title}</h2>
 			<h4>{pizza.price} ₽</h4>
+			<Link to="/">
+				<button style={{marginTop: '15px'}} className="button button--outline button--add">
+					<span>Назад</span>
+				</button>
+			</Link>
 		</div>
 	);
 };
